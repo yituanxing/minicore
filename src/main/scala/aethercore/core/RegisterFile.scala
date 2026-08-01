0bbb69111f4fc39a908efd8b69c1dbfc9fc492f9
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-01 23:23:48 +0800

    parameterize register-file width

--- src/main/scala/aethercore/core/RegisterFile.scala
+++ src/main/scala/aethercore/core/RegisterFile.scala
@@ -3,19 +3,21 @@ package aethercore.core
 import chisel3._
 import chisel3.util._
 
-class RegisterFile extends Module {
+class RegisterFile(val xlen: Int = 64) extends Module {
+  require(xlen == 32 || xlen == 64, s"register-file XLEN must be 32 or 64, got $xlen")
+
   val io = IO(new Bundle {
     val rs1Addr = Input(UInt(5.W))
     val rs2Addr = Input(UInt(5.W))
-    val rs1Data = Output(UInt(64.W))
-    val rs2Data = Output(UInt(64.W))
+    val rs1Data = Output(UInt(xlen.W))
+    val rs2Data = Output(UInt(xlen.W))
 
     val writeEnable = Input(Bool())
     val rdAddr = Input(UInt(5.W))
-    val rdData = Input(UInt(64.W))
+    val rdData = Input(UInt(xlen.W))
   })
 
-  val regs = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
+  val regs = RegInit(VecInit(Seq.fill(32)(0.U(xlen.W))))
   val doWrite = io.writeEnable && io.rdAddr =/= 0.U
 
   when(doWrite) {
@@ -24,12 +26,12 @@ class RegisterFile extends Module {
 
   io.rs1Data := Mux(
     io.rs1Addr === 0.U,
-    0.U,
+    0.U(xlen.W),
     Mux(doWrite && io.rdAddr === io.rs1Addr, io.rdData, regs(io.rs1Addr))
   )
   io.rs2Data := Mux(
     io.rs2Addr === 0.U,
-    0.U,
+    0.U(xlen.W),
     Mux(doWrite && io.rdAddr === io.rs2Addr, io.rdData, regs(io.rs2Addr))
   )
 }
