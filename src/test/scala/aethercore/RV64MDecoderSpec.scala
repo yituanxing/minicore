70adacaac9565c05874ff484e35bff26dd4d03ec
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-01 19:07:42 +0800

    test: require exact RV64M ALU identities

--- src/test/scala/aethercore/RV64MDecoderSpec.scala
+++ src/test/scala/aethercore/RV64MDecoderSpec.scala
@@ -4,27 +4,34 @@ import chisel3._
 import chisel3.simulator.scalatest.ChiselSim
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import aethercore.common.AluOp
 import aethercore.core.Decoder
 
 class RV64MDecoderSpec extends AnyFlatSpec with Matchers with ChiselSim {
   behavior of "RV64M Decoder"
 
-  private case class DecodeCase(name: String, opcode: Int, funct3: Int, wordOp: Boolean)
+  private case class DecodeCase(
+      name: String,
+      opcode: Int,
+      funct3: Int,
+      aluOp: AluOp.Type,
+      wordOp: Boolean
+  )
 
   private val cases = Seq(
-    DecodeCase("MUL",    0x33, 0x0, false),
-    DecodeCase("MULH",   0x33, 0x1, false),
-    DecodeCase("MULHSU", 0x33, 0x2, false),
-    DecodeCase("MULHU",  0x33, 0x3, false),
-    DecodeCase("DIV",    0x33, 0x4, false),
-    DecodeCase("DIVU",   0x33, 0x5, false),
-    DecodeCase("REM",    0x33, 0x6, false),
-    DecodeCase("REMU",   0x33, 0x7, false),
-    DecodeCase("MULW",   0x3b, 0x0, true),
-    DecodeCase("DIVW",   0x3b, 0x4, true),
-    DecodeCase("DIVUW",  0x3b, 0x5, true),
-    DecodeCase("REMW",   0x3b, 0x6, true),
-    DecodeCase("REMUW",  0x3b, 0x7, true)
+    DecodeCase("MUL",    0x33, 0x0, AluOp.Mul,    false),
+    DecodeCase("MULH",   0x33, 0x1, AluOp.Mulh,   false),
+    DecodeCase("MULHSU", 0x33, 0x2, AluOp.Mulhsu, false),
+    DecodeCase("MULHU",  0x33, 0x3, AluOp.Mulhu,  false),
+    DecodeCase("DIV",    0x33, 0x4, AluOp.Div,    false),
+    DecodeCase("DIVU",   0x33, 0x5, AluOp.Divu,   false),
+    DecodeCase("REM",    0x33, 0x6, AluOp.Rem,    false),
+    DecodeCase("REMU",   0x33, 0x7, AluOp.Remu,   false),
+    DecodeCase("MULW",   0x3b, 0x0, AluOp.Mul,    true),
+    DecodeCase("DIVW",   0x3b, 0x4, AluOp.Div,    true),
+    DecodeCase("DIVUW",  0x3b, 0x5, AluOp.Divu,   true),
+    DecodeCase("REMW",   0x3b, 0x6, AluOp.Rem,    true),
+    DecodeCase("REMUW",  0x3b, 0x7, AluOp.Remu,   true)
   )
 
   private def encodeR(opcode: Int, funct3: Int): BigInt = {
@@ -42,11 +49,12 @@ class RV64MDecoderSpec extends AnyFlatSpec with Matchers with ChiselSim {
     )
   }
 
-  it should "accept all RV64M register encodings with the correct width class" in {
+  it should "accept all RV64M register encodings with the exact operation and width class" in {
     simulate(new Decoder) { dut =>
       for (test <- cases) {
         withClue(test.name) {
           dut.io.inst.poke(encodeR(test.opcode, test.funct3).U(32.W))
+          dut.io.ctrl.aluOp.expect(test.aluOp)
           dut.io.ctrl.illegal.expect(false.B)
           dut.io.ctrl.regWrite.expect(true.B)
           dut.io.ctrl.usesRs1.expect(true.B)
