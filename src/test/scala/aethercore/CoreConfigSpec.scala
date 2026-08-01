82f396f7af2742a73b9f43138a49af968c15f9b1
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-01 23:01:25 +0800

    test current core profile and invalid configurations

---
+++ src/test/scala/aethercore/CoreConfigSpec.scala
@@ -0,0 +1,54 @@
+package aethercore
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import aethercore.config.{CoreProfiles, IsaConfig, PlatformConfig}
+
+class CoreConfigSpec extends AnyFlatSpec with Matchers {
+  behavior of "CoreConfig"
+
+  it should "describe the exact current RV64IM software contract" in {
+    val config = CoreProfiles.rv64imCurrent
+
+    config.name shouldBe "rv64im-current"
+    config.isa.xlen shouldBe 64
+    config.isa.xBytes shouldBe 8
+    config.isa.shiftBits shouldBe 6
+    config.isa.hasM shouldBe true
+    config.isa.hasA shouldBe false
+    config.isa.hasWordOps shouldBe true
+    config.isa.march shouldBe "rv64im"
+    config.isa.mabi shouldBe "lp64"
+    config.platform.resetVector shouldBe BigInt("80000000", 16)
+    config.platform.busDataBits shouldBe 64
+    config.platform.busBytes shouldBe 8
+  }
+
+  it should "derive a future RV32I software contract without enabling it in RTL" in {
+    val isa = IsaConfig(
+      xlen = 32,
+      extensions = Set('I'),
+      privilegeModes = Set('M')
+    )
+
+    isa.xBytes shouldBe 4
+    isa.shiftBits shouldBe 5
+    isa.hasWordOps shouldBe false
+    isa.march shouldBe "rv32i"
+    isa.mabi shouldBe "ilp32"
+  }
+
+  it should "reject unsupported architectural and platform widths early" in {
+    an[IllegalArgumentException] should be thrownBy
+      IsaConfig(xlen = 48, extensions = Set('I'), privilegeModes = Set('M'))
+
+    an[IllegalArgumentException] should be thrownBy
+      PlatformConfig(
+        resetVector = 0,
+        paddrBits = 40,
+        busDataBits = 128,
+        uartAddress = 0,
+        exitAddress = 8
+      )
+  }
+}
