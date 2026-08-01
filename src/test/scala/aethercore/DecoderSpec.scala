32724da1350bca2ed850b421e8fa3bbcac6b66bb
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-01 23:35:41 +0800

    test RV32 and extension-aware decoding

--- src/test/scala/aethercore/DecoderSpec.scala
+++ src/test/scala/aethercore/DecoderSpec.scala
@@ -5,6 +5,7 @@ import chisel3.simulator.scalatest.ChiselSim
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import aethercore.common._
+import aethercore.config.IsaConfig
 import aethercore.core.Decoder
 
 class DecoderSpec extends AnyFlatSpec with Matchers with ChiselSim {
@@ -25,9 +26,61 @@ class DecoderSpec extends AnyFlatSpec with Matchers with ChiselSim {
       dut.io.ctrl.memWrite.expect(true.B)
       dut.io.ctrl.memSize.expect(MemSize.Byte)
 
+      // SLLI with shamt[5] set is legal in RV64 but not RV32.
+      dut.io.inst.poke("h02001013".U)
+      dut.io.ctrl.illegal.expect(false.B)
+      dut.io.ctrl.aluOp.expect(AluOp.Sll)
+
       dut.io.inst.poke("h00100073".U)
       dut.io.ctrl.illegal.expect(false.B)
       dut.io.ctrl.trap.expect(true.B)
+    }
+  }
+
+  it should "exclude RV64-only encodings from RV32I" in {
+    val rv32i = IsaConfig(
+      xlen = 32,
+      extensions = Set('I'),
+      privilegeModes = Set('M')
+    )
+
+    simulate(new Decoder(rv32i)) { dut =>
+      dut.io.inst.poke("h00002083".U) // lw x1, 0(x0)
+      dut.io.ctrl.illegal.expect(false.B)
+      dut.io.ctrl.memSize.expect(MemSize.Word)
+
+      dut.io.inst.poke("h00003083".U) // ld x1, 0(x0)
+      dut.io.ctrl.illegal.expect(true.B)
+
+      dut.io.inst.poke("h00006083".U) // lwu x1, 0(x0)
+      dut.io.ctrl.illegal.expect(true.B)
+
+      dut.io.inst.poke("h00103023".U) // sd x1, 0(x0)
+      dut.io.ctrl.illegal.expect(true.B)
+
+      dut.io.inst.poke("h0010809b".U) // addiw x1, x1, 1
+      dut.io.ctrl.illegal.expect(true.B)
+
+      dut.io.inst.poke("h02001013".U) // RV64 slli shamt=32
+      dut.io.ctrl.illegal.expect(true.B)
+
+      dut.io.inst.poke("h022081b3".U) // mul x3, x1, x2
+      dut.io.ctrl.illegal.expect(true.B)
+    }
+  }
+
+  it should "enable M instructions only when configured" in {
+    val rv32im = IsaConfig(
+      xlen = 32,
+      extensions = Set('I', 'M'),
+      privilegeModes = Set('M')
+    )
+
+    simulate(new Decoder(rv32im)) { dut =>
+      dut.io.inst.poke("h022081b3".U)
+      dut.io.ctrl.illegal.expect(false.B)
+      dut.io.ctrl.aluOp.expect(AluOp.Mul)
+      dut.io.ctrl.wordOp.expect(false.B)
     }
   }
 }
