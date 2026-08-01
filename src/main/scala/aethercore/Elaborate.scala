5bfae19a72ffdaadf402f00536aaa4b8f8fac6f9
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-01 23:01:03 +0800

    elaborate the named current core profile

--- src/main/scala/aethercore/Elaborate.scala
+++ src/main/scala/aethercore/Elaborate.scala
@@ -1,11 +1,12 @@
 package aethercore
 
 import _root_.circt.stage.ChiselStage
+import aethercore.config.CoreProfiles
 import aethercore.sim.AetherCoreSimTop
 
 object Elaborate extends App {
   ChiselStage.emitSystemVerilogFile(
-    new AetherCoreSimTop,
+    new AetherCoreSimTop(CoreProfiles.rv64imCurrent),
     args,
     Array(
       "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
