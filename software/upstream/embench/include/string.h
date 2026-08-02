801fdd68fdb426acbb3732509cf6915491322f57
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-02 11:36:50 +0800

    extend the freestanding string contract

--- software/upstream/embench/include/string.h
+++ software/upstream/embench/include/string.h
@@ -8,6 +8,7 @@ void *memmove(void *dest, const void *src, size_t count);
 void *memset(void *dest, int value, size_t count);
 int memcmp(const void *lhs, const void *rhs, size_t count);
 void *memchr(const void *ptr, int value, size_t count);
+char *strchr(const char *text, int value);
 size_t strlen(const char *text);
 int strcmp(const char *lhs, const char *rhs);
 int strncmp(const char *lhs, const char *rhs, size_t count);
