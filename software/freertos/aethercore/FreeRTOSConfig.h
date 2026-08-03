165f18e312eeed1d31a867886be3694fd09b81bd
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-03 22:25:38 +0800

    feat: enable FreeRTOS idle hook

--- software/freertos/aethercore/FreeRTOSConfig.h
+++ software/freertos/aethercore/FreeRTOSConfig.h
@@ -30,7 +30,7 @@ void aether_assert_fail( const char * file, int line );
 #define configGENERATE_RUN_TIME_STATS           0
 #define configCHECK_FOR_STACK_OVERFLOW          2
 #define configUSE_MALLOC_FAILED_HOOK            1
-#define configUSE_IDLE_HOOK                     0
+#define configUSE_IDLE_HOOK                     1
 #define configUSE_TICK_HOOK                     0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 #define configSUPPORT_STATIC_ALLOCATION         0
