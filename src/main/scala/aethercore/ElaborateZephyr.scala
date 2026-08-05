60eb9081e5d8b878167901be35be7bc79a53862b
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-05 09:33:13 +0800

    fix: elaborate the Zephyr RV32 runtime profile

---
+++ src/main/scala/aethercore/ElaborateZephyr.scala
@@ -0,0 +1,19 @@
+package aethercore
+
+import _root_.circt.stage.ChiselStage
+import aethercore.config.CoreProfiles
+import aethercore.sim.AetherCoreSimTop
+
+object ElaborateZephyr extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new AetherCoreSimTop(
+      config = CoreProfiles.rv32imSoftware,
+      stopOnTrap = false,
+      withMachineInterruptPlatform = false
+    ),
+    args,
+    Array(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
+    )
+  )
+}
