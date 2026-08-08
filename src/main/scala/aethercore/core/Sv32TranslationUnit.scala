6b587e53951ee2699017f99416abf985d611b6f2
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-08 13:44:46 +0800

    feat(sv32): integrate TLB into translation unit / 将TLB接入地址翻译单元

--- src/main/scala/aethercore/core/Sv32TranslationUnit.scala
+++ src/main/scala/aethercore/core/Sv32TranslationUnit.scala
@@ -4,14 +4,15 @@ import chisel3._
 import chisel3.util._
 import aethercore.common.PrivilegeMode
 
-/** Composes Sv32 activation rules with the two-level walker. */
-class Sv32TranslationUnit extends Module {
+/** Composes Sv32 activation rules, a bounded TLB, and the two-level walker. */
+class Sv32TranslationUnit(val tlbEntries: Int = 8) extends Module {
   private val PaddrBits = 34
 
   val io = IO(new Bundle {
     val requestValid = Input(Bool())
     val requestReady = Output(Bool())
     val kill = Input(Bool())
+    val flush = Input(Bool())
     val virtualAddress = Input(UInt(32.W))
     val privilege = Input(UInt(2.W))
     val write = Input(Bool())
@@ -37,16 +38,56 @@ class Sv32TranslationUnit extends Module {
     val global = Output(Bool())
   })
 
-  val idle :: walking :: bypassResponse :: Nil = Enum(3)
+  val idle :: walking :: bypassResponse :: tlbResponse :: Nil = Enum(4)
   val state = RegInit(idle)
   val bypassPhysicalAddress = RegInit(0.U(PaddrBits.W))
+  val cachedPhysicalAddress = RegInit(0.U(PaddrBits.W))
+  val cachedLeafLevel = RegInit(0.U(1.W))
+  val cachedGlobal = RegInit(false.B)
+
+  // A miss keeps the accepted architectural context stable until the walker
+  // response is consumed and committed into the TLB.
+  val requestVirtualAddress = Reg(UInt(32.W))
+  val requestRootPpn = Reg(UInt(Sv32Satp.PpnBits.W))
+  val requestPrivilege = Reg(UInt(2.W))
+  val requestWrite = Reg(Bool())
+  val requestExecute = Reg(Bool())
+  val requestSum = Reg(Bool())
+  val requestMxr = Reg(Bool())
 
   val walker = Module(new Sv32PageTableWalker)
+  val tlb = Module(new Sv32Tlb(tlbEntries))
+  val abort = io.kill || io.flush
   val translationRequired =
     io.satpTranslationEnabled && io.privilege =/= PrivilegeMode.Machine.U
+  val lookupActive = state === idle && io.requestValid && translationRequired && !abort
 
-  walker.io.requestValid := state === idle && io.requestValid && translationRequired
-  walker.io.kill := io.kill
+  tlb.io.lookupValid := lookupActive
+  tlb.io.virtualAddress := io.virtualAddress
+  tlb.io.rootPpn := io.satpRootPpn
+  tlb.io.privilege := io.privilege
+  tlb.io.write := io.write
+  tlb.io.execute := io.execute
+  tlb.io.sum := io.sum
+  tlb.io.mxr := io.mxr
+  tlb.io.flush := io.flush
+
+  val refill = state === walking && walker.io.responseValid && io.responseReady &&
+    !walker.io.pageFault && !walker.io.accessFault && !abort
+  tlb.io.refillValid := refill
+  tlb.io.refillVirtualAddress := requestVirtualAddress
+  tlb.io.refillPhysicalAddress := walker.io.physicalAddress
+  tlb.io.refillRootPpn := requestRootPpn
+  tlb.io.refillPrivilege := requestPrivilege
+  tlb.io.refillWrite := requestWrite
+  tlb.io.refillExecute := requestExecute
+  tlb.io.refillSum := requestSum
+  tlb.io.refillMxr := requestMxr
+  tlb.io.refillLeafLevel := walker.io.leafLevel
+  tlb.io.refillGlobal := walker.io.global
+
+  walker.io.requestValid := lookupActive && !tlb.io.hit
+  walker.io.kill := abort
   walker.io.virtualAddress := io.virtualAddress
   walker.io.rootPpn := io.satpRootPpn
   walker.io.privilege := io.privilege
@@ -57,12 +98,15 @@ class Sv32TranslationUnit extends Module {
   walker.io.pteReady := io.pteReady
   walker.io.pteData := io.pteData
   walker.io.pteFault := io.pteFault
-  walker.io.responseReady := state === walking && io.responseReady
+  walker.io.responseReady := state === walking && io.responseReady && !abort
 
-  io.pteValid := walker.io.pteValid && !io.kill
+  io.pteValid := walker.io.pteValid && !abort
   io.pteAddress := walker.io.pteAddress
-  io.requestReady := state === idle && !io.kill &&
-    Mux(translationRequired, walker.io.requestReady, true.B)
+  io.requestReady := state === idle && !abort && Mux(
+    translationRequired,
+    Mux(tlb.io.hit, true.B, walker.io.requestReady),
+    true.B
+  )
 
   io.responseValid := false.B
   io.physicalAddress := 0.U
@@ -72,23 +116,45 @@ class Sv32TranslationUnit extends Module {
   io.global := false.B
 
   when(state === walking) {
-    io.responseValid := walker.io.responseValid && !io.kill
+    io.responseValid := walker.io.responseValid && !abort
     io.physicalAddress := walker.io.physicalAddress
     io.pageFault := walker.io.pageFault
     io.accessFault := walker.io.accessFault
     io.leafLevel := walker.io.leafLevel
     io.global := walker.io.global
   }.elsewhen(state === bypassResponse) {
-    io.responseValid := !io.kill
+    io.responseValid := !abort
     io.physicalAddress := bypassPhysicalAddress
+  }.elsewhen(state === tlbResponse) {
+    io.responseValid := !abort
+    io.physicalAddress := cachedPhysicalAddress
+    io.leafLevel := cachedLeafLevel
+    io.global := cachedGlobal
   }
 
-  when(io.kill) {
+  when(abort) {
     state := idle
     bypassPhysicalAddress := 0.U
+    cachedPhysicalAddress := 0.U
+    cachedLeafLevel := 0.U
+    cachedGlobal := false.B
   }.elsewhen(state === idle && io.requestValid && io.requestReady) {
     when(translationRequired) {
-      state := walking
+      when(tlb.io.hit) {
+        cachedPhysicalAddress := tlb.io.physicalAddress
+        cachedLeafLevel := tlb.io.leafLevel
+        cachedGlobal := tlb.io.global
+        state := tlbResponse
+      }.otherwise {
+        requestVirtualAddress := io.virtualAddress
+        requestRootPpn := io.satpRootPpn
+        requestPrivilege := io.privilege
+        requestWrite := io.write
+        requestExecute := io.execute
+        requestSum := io.sum
+        requestMxr := io.mxr
+        state := walking
+      }
     }.otherwise {
       bypassPhysicalAddress := Cat(0.U(2.W), io.virtualAddress)
       state := bypassResponse
@@ -96,6 +162,8 @@ class Sv32TranslationUnit extends Module {
   }.elsewhen(state === walking && walker.io.responseValid && io.responseReady) {
     state := idle
   }.elsewhen(state === bypassResponse && io.responseReady) {
+    state := idle
+  }.elsewhen(state === tlbResponse && io.responseReady) {
     state := idle
   }
 }
