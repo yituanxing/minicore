6d08a4b917a0e03993699901816d2aba80a0b6d8
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-15 17:51:19 +0800

    refactor(scope): keep Linux profile in stacked frontier

--- src/main/scala/aethercore/sim/AetherCoreOpenSbiSimTop.scala
+++ src/main/scala/aethercore/sim/AetherCoreOpenSbiSimTop.scala
@@ -11,7 +11,7 @@ import aethercore.config.CoreProfiles
   */
 class AetherCoreOpenSbiSimTop
     extends AetherCoreSimTop(
-      config = CoreProfiles.rv32imasuSv32PmpSoftware,
+      config = CoreProfiles.rv32imasuSv32Software,
       stopOnTrap = false,
       withMachineInterruptPlatform = false,
       withSupervisorInterruptPlatform = true,
