b8a55abc3215208ec2acadc018e3f85a6fcfbbbc
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-25 19:47:01 +0800

    perf(v2): elaborate host-visible causal attribution

--- src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
+++ src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
@@ -1,12 +1,12 @@
 package aethercore
 
 import _root_.circt.stage.ChiselStage
-import aethercore.sim.AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisible
+import aethercore.sim.AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttribution
 
 /** Elaboration entry for the F7 v2 core on the qualified RV64 OpenSBI board. */
 object ElaborateV2OpenSbiRV64 extends App {
   ChiselStage.emitSystemVerilogFile(
-    new AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisible,
+    new AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttribution,
     args,
     Array(
       "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
