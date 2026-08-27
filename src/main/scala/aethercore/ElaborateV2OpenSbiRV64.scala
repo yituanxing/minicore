76a80092b6dfc7d11b3c59fa1701f7a6bf85908b
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-27 09:01:08 +0800

    perf(v2): elaborate opportunity attribution top

--- src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
+++ src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
@@ -1,7 +1,7 @@
 package aethercore
 
 import _root_.circt.stage.ChiselStage
-import aethercore.sim.AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttributionV11
+import aethercore.sim.AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttributionV12
 
 /** Elaboration entry for the F7 v2 core on the qualified RV64 OpenSBI board. */
 object ElaborateV2OpenSbiRV64 extends App {
