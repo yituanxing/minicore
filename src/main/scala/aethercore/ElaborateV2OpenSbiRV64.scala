faf9197aaef1fdb3dd7ff6e4133a9adcb5cb3a20
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-27 09:13:43 +0800

    fix(v2): instantiate opportunity attribution top

--- src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
+++ src/main/scala/aethercore/ElaborateV2OpenSbiRV64.scala
@@ -6,7 +6,7 @@ import aethercore.sim.AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttributio
 /** Elaboration entry for the F7 v2 core on the qualified RV64 OpenSBI board. */
 object ElaborateV2OpenSbiRV64 extends App {
   ChiselStage.emitSystemVerilogFile(
-    new AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttributionV11,
+    new AetherCoreV2MeasuredOpenSbiRV64SimTopHostVisibleAttributionV12,
     args,
     Array(
       "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
