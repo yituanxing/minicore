f9e9170730f632880bb7089bea6cf7dc2ef15098
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-29 12:23:23 +0800

    test: require standard DTS label references

--- src/test/scala/aethercore/AetherSoCDtsSpec.scala
+++ src/test/scala/aethercore/AetherSoCDtsSpec.scala
@@ -28,15 +28,20 @@ class AetherSoCDtsSpec extends AnyFlatSpec with Matchers {
 
     dts should include(s"riscv,ndev = <${board.plicSourceCount}>;")
     dts should include(s"interrupts = <${board.uartPlicSourceId}>;")
+    dts should include("cpu_intc: interrupt-controller {")
+    dts should include(s"plic: interrupt-controller@${map.plicBase.toString(16)} {")
     dts should include(
-      s"interrupts-extended = <1 0xffffffff 1 ${board.supervisorExternalInterruptId}>;"
+      s"interrupts-extended = <&cpu_intc 0xffffffff &cpu_intc ${board.supervisorExternalInterruptId}>;"
     )
+    dts should include("interrupt-parent = <&plic>;")
     dts should include(
-      s"interrupts-extended = <1 ${board.machineTimerInterruptId}>;"
+      s"interrupts-extended = <&cpu_intc ${board.machineTimerInterruptId}>;"
     )
     dts should include(s"timebase-frequency = <${board.timebaseFrequencyHz}>;")
     dts should include(s"clock-frequency = <${board.uartClockFrequencyHz}>;")
     dts should include("bootargs = \"console=ttyS0 rdinit=/init\";")
+    dts should not include "phandle = <1>;"
+    dts should not include "phandle = <2>;"
   }
 
   it should "fail closed for a non-RV64 or non-Sv39 software profile" in {
