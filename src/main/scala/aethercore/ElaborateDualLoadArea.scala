df8325903ff988ac372f666407bb29b80ab16c90
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-30 13:02:16 +0800

    measure: split LSU translation and PMP cost

--- src/main/scala/aethercore/ElaborateDualLoadArea.scala
+++ src/main/scala/aethercore/ElaborateDualLoadArea.scala
@@ -2,6 +2,7 @@ package aethercore
 
 import _root_.circt.stage.ChiselStage
 import aethercore.config.PageTableGeometry
+import aethercore.core.{DataPathAdapter, PmpChecker, PmpConstants}
 import aethercore.core.v2.{TinyBlockingLsu, TinyDualReplaySafeLoadUnit}
 
 object ElaborateTinyBlockingLsuSynthesisRV64 extends App {
@@ -25,6 +26,31 @@ object ElaborateTinyDualReplaySafeLoadUnitSynthesisRV64 extends App {
       paddrBits = PageTableGeometry.Sv39.architecturalPhysicalAddressBits,
       tlbEntries = 8,
       txnIdBits = 2
+    ),
+    args,
+    Array("--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket")
+  )
+}
+
+
+object ElaborateDataPathAdapterSynthesisRV64 extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new DataPathAdapter(
+      PageTableGeometry.Sv39,
+      paddrBits = PageTableGeometry.Sv39.architecturalPhysicalAddressBits,
+      tlbEntries = 8
+    ),
+    args,
+    Array("--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket")
+  )
+}
+
+object ElaboratePmpCheckerSynthesisRV64Pa56 extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new PmpChecker(
+      xlen = 64,
+      entries = PmpConstants.MaxEntries,
+      paddrBits = PageTableGeometry.Sv39.architecturalPhysicalAddressBits
     ),
     args,
     Array("--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket")
