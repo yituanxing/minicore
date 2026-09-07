b4b32c9499a6fd7b0187fe9510fa9c6005c479f9
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-07 12:11:15 +0800

    perf: promote 1 KiB D-cache candidate

--- src/main/scala/aethercore/soc/AetherCoreV2LinuxSoC.scala
+++ src/main/scala/aethercore/soc/AetherCoreV2LinuxSoC.scala
@@ -115,7 +115,7 @@ class AetherCoreV2LinuxSoC(
     config,
     geometry,
     txnIdBits = txnIdBits,
-    dcacheEntries = 64,
+    dcacheEntries = 128,
     enableInstructionBackpressure = enableInstructionBackpressure
   ))
 
