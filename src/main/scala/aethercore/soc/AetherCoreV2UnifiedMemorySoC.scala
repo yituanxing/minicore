307193d3930c53ba5ce20bbaef4303ad573d4bde
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-08 00:49:49 +0800

    measure: add elaboration-time safe full-beat switch

--- src/main/scala/aethercore/soc/AetherCoreV2UnifiedMemorySoC.scala
+++ src/main/scala/aethercore/soc/AetherCoreV2UnifiedMemorySoC.scala
@@ -20,7 +20,8 @@ import aethercore.memory.{AetherMemRequest, AetherMemResponse}
   */
 class AetherCoreV2UnifiedMemorySoC(
     val externalPhysicalSeams: Boolean = false,
-    val implementedPaddrBits: Int = 56
+    val implementedPaddrBits: Int = 56,
+    val enableSafeFullBeatIcache: Boolean = true
 ) extends Module {
   private val xlen = 64
   private val paddrBits = implementedPaddrBits
@@ -140,8 +141,12 @@ class AetherCoreV2UnifiedMemorySoC(
     imemStart >= addressMap.ramBase.U &&
       imemBeatEndExclusive <= addressMap.ramLimit.U
   instructionCache.io.frontendFullBeatAllowed :=
-    platform.io.imemFullBeatPmpSafe &&
-      (fullBeatInBootRom || fullBeatInRam)
+    (if (enableSafeFullBeatIcache) {
+      platform.io.imemFullBeatPmpSafe &&
+        (fullBeatInBootRom || fullBeatInRam)
+    } else {
+      false.B
+    })
 
   instructionCache.io.invalidateAll := platform.io.instructionFence
   platform.io.imemReady.get := instructionCache.io.frontendReady
